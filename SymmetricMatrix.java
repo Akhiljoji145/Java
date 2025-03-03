3fce9f8bfd4842060d75853db60745e98560de86
Akhiljoji145 <akhiljoji1451@gmail.com>
2025-03-03 16:06:40 +0530

    modified

--- SymmetricMatrix.java
+++ SymmetricMatrix.java
@@ -4,27 +4,24 @@ public class SymmetricMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of the matrix");
-        int n = sc.nextInt(); // Read matrix size (assuming square matrix)
+        int n = sc.nextInt();
         int[][] matrix = new int[n][n];
 
-        // Read matrix elements
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 matrix[i][j] = sc.nextInt();
 
-        // Check symmetry with early termination
         boolean symmetric = true;
         outerLoop:
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) { // Check only lower triangle
+            for (int j = 0; j < i; j++) {
                 if (matrix[i][j] != matrix[j][i]) {
                     symmetric = false;
-                    break outerLoop; // Exit immediately on mismatch
+                    break outerLoop; 
                 }
             }
         }
-
-        // Print result
         System.out.println(symmetric ? "Symmetric" : "Not Symmetric");
 
         sc.close();
