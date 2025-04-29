c6de91cf76d570cff9d3bcfea48c2ae614fc14fd
Amal070 <amalshaji7cr7@gmail.com>
2025-04-29 11:01:06 -0700

    modified

---
+++ Graphics/Circle.java
@@ -0,0 +1,10 @@
+package Graphics;
+public class Circle {
+double radius;
+public Circle(double radius) {
+this.radius = radius;
+}
+public double calculateArea() {
+return Math.PI * radius * radius;
+}
+}
---
+++ Graphics/Square.java
@@ -0,0 +1,12 @@
+package Graphics;
+
+public class Square {
+double side;
+public Square(double side) {
+this.side = side;
+}
+
+public double calculateArea() {
+return side * side;
+}
+}
